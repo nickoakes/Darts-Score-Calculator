76a502150ead7c4717e967a6792ad34b10ea2676
Nicholas Oakes <32389274+nickoakes@users.noreply.github.com>
2019-05-13 15:35:34 +0100

    Additional comments added

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+/*
+	Darts Score Calculator
+	app.js
+*/
+
 $(document).ready(function(){
 	
 	//name entry
 	
 	var p1Name = prompt("Player 1 please enter your name:");
 	var p2Name = prompt("Player 2 please enter your name:");
+
+//dynamically set button values using names entered
 
 	function setNames(){
 	$('#p1ScoreName').text(p1Name);
@@ -18,11 +25,17 @@ $(document).ready(function(){
 	$("td").on("click", function(){
 		clickedDart = $(this).attr("data");
 		clickedDart = parseInt(clickedDart);
+
+//player 1 current score over fifty
+
 			if($('#p1Score').is('.highlight') && clickedDart && ((document.getElementById('p1Score').innerHTML - clickedDart) > 50)){
 				p1Current = document.getElementById('p1Score').innerHTML;
 				p1Current = parseInt(p1Current);
 				document.getElementById('p1Score').innerHTML = p1Current - clickedDart;
 			}
+
+//player 1 on a double
+
 			else if($('#p1Score').is('.highlight') && clickedDart && 2 < (document.getElementById('p1Score').innerHTML - clickedDart) && (document.getElementById('p1Score').innerHTML - clickedDart) <= 49 && !((document.getElementById("p1Score").innerHTML - clickedDart) % 2)) {
 				p1Current = document.getElementById('p1Score').innerHTML;
 				p1Current = parseInt(p1Current);
@@ -31,6 +44,9 @@ $(document).ready(function(){
 				$('#p1Score').hide();
 				$('#p1Double').show();
 			}
+
+//player 1 on bull checkout
+
 			else if((document.getElementById('p1Score').innerHTML - clickedDart) === 50){
 				p1Current = document.getElementById('p1Score').innerHTML;
 				p1Current = parseInt(p1Current);
@@ -39,16 +55,25 @@ $(document).ready(function(){
 				$('#p1Score').hide();
 				$('#p1Double').show();
 			}
+
+//player 1 current score under 50, but not a double
+
 			else if($('#p1Score').is('.highlight') && clickedDart && 2 < (document.getElementById('p1Score').innerHTML - clickedDart) && (document.getElementById('p1Score').innerHTML - clickedDart) <= 49 && ((document.getElementById("p1Score").innerHTML - clickedDart) % 2)){
 				p1Current = document.getElementById('p1Score').innerHTML;
 				p1Current = parseInt(p1Current);
 				document.getElementById('p1Score').innerHTML = p1Current - clickedDart;
 			}
+
+//player 2 current score over 50
+
 			else if($('#p2Score').is('.highlight') && clickedDart && ((document.getElementById('p2Score').innerHTML - clickedDart) > 50)){
 				p2Current = document.getElementById('p2Score').innerHTML;
 				p2Current = parseInt(p2Current);
 				document.getElementById('p2Score').innerHTML = p2Current - clickedDart;
 			} 
+
+//player 2 on a double
+
 			else if($('#p2Score').is('.highlight') && clickedDart && 2 < (document.getElementById('p2Score').innerHTML - clickedDart) && (document.getElementById('p2Score').innerHTML - clickedDart) <= 49 && !((document.getElementById("p2Score").innerHTML - clickedDart) % 2)) {
 				p2Current = document.getElementById('p2Score').innerHTML;
 				p2Current = parseInt(p2Current);
@@ -57,6 +82,9 @@ $(document).ready(function(){
 				$('#p2Score').hide();
 				$('#p2Double').show();
 			}
+
+//player 2 on bull checkout
+
 			else if((document.getElementById('p2Score').innerHTML - clickedDart) === 50){
 				p2Current = document.getElementById('p2Score').innerHTML;
 				p2Current = parseInt(p2Current);
@@ -65,17 +93,26 @@ $(document).ready(function(){
 				$('#p2Score').hide();
 				$('#p2Double').show();
 			}
+
+//player 2 current score under 50, but not a double
+
 			else if($('#p2Score').is('.highlight') && clickedDart && 2 < (document.getElementById('p2Score').innerHTML - clickedDart) && (document.getElementById('p2Score').innerHTML - clickedDart) <= 49 && ((document.getElementById("p2Score").innerHTML - clickedDart) % 2)){
 				p2Current = document.getElementById('p2Score').innerHTML;
 				p2Current = parseInt(p2Current);
 				document.getElementById('p2Score').innerHTML = p2Current - clickedDart;
 			}
+
+//player's dart greater than current score, leading them to bust
+
 			else if($('#p1Score').is('.highlight') && clickedDart && (document.getElementById('p1Score').innerHTML - clickedDart) < 2 && (document.getElementById('p1Score').innerHTML - clickedDart) != 0) {
 				alert("Bust!");
 			}
 			else if($('#p2Score').is('.highlight') && clickedDart && (document.getElementById('p2Score').innerHTML - clickedDart) < 2 && (document.getElementById('p2Score').innerHTML - clickedDart) != 0) {
 				alert("Bust!");
 			}
+
+//player successfully hits double and checks out
+
 			else if((document.getElementById('p1Score').innerHTML - clickedDart) === 0) {
 				alert("Game " + p1Name + "!");
 				$('#p1Score').text(0);
@@ -149,6 +186,8 @@ $(document).ready(function(){
 		$(this).animate({fontSize: "20px", opacity: "0.8"}, 200);
 	});
 	
+//show Bully when mouse hovers over bull
+
 	$('.bull').on("mouseenter", function(){
 		$('#winmau').hide(1)
 		$('#bully').show(200);
@@ -159,6 +198,8 @@ $(document).ready(function(){
 		$('#winmau').show(200);
 	});
 	
+//show Phil Taylor when mouse hovers over treble 20
+
 	$('#t20').on('mouseenter', function(){
 		$('#winmau').hide(1);
 		$('#phil').show(200);
@@ -168,6 +209,8 @@ $(document).ready(function(){
 		$('#phil').hide(1);
 		$('#winmau').show(200);
 	});
+
+//show dyson vaccuum when a player throws 'tidy' darts, causing their current score to be a multiple of 100
 	
 	$('td').on("click", function(){if(!(document.getElementById('p1Score').innerHTML % 100) || !(document.getElementById('p2Score').innerHTML % 100)){
 		$('#winmau').hide(1);
@@ -178,6 +221,8 @@ $(document).ready(function(){
 	}
 	});
 	
+//reveal link to checkout chart when a player's current score is at or below the maximum 3-dart checkout of 170
+
 	$('td').on("click", function(){
 	if (document.getElementById('p1Score').innerHTML < 171 || document.getElementById('p2Score').innerHTML < 171){
 	$('#checkout').show(1);
